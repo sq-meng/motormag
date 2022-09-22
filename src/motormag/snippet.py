ae9a44a2c2a49ec9c107421fa1b61d3d6f2a44d8
Siqin Meng <mengsq04@gmail.com>
2022-09-22 23:19:18 +0800

    minor edit

--- src/motormag/snippet.py
+++ src/motormag/snippet.py
@@ -1,4 +1,4 @@
 import motormag
 
-df = motormag.scan.box_scan([10, 20], [20, 40], [40, 70])
+df = motormag.scan.box_scan([10, 20], [20, 40], [40, 70], n_discards=0)
 motormag.draw.strength_2d(df, 'y')
